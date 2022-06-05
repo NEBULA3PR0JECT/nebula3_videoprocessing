48bfb105c1088ea95edaf0d988d3c6e7b5fb876c
michael kolomenkin <michael.kolomenkin@gmail.com>
2022-06-05 15:16:12 +0300

    opened a new branch and MDFSelection class

---
+++ videoprocessing/mdf_selection.py
@@ -0,0 +1,11 @@
+
+class MDFSelection:
+    """
+    The class is responsible for choosing the MVP from a video
+    """
+    def __int__(self):
+        pass
+
+
+if __name__ == '__main__':
+    print('Test MDF selection')
